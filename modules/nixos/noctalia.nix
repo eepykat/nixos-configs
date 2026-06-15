24eb51d8c7bdcf495229ae3bd5ccbf9dec91508a
eepykat <168898505+eepykat@users.noreply.github.com>
2026-06-15 16:25:17 +0300

    fix

--- modules/nixos/noctalia.nix
+++ modules/nixos/noctalia.nix
@@ -1,22 +1,20 @@
-{ pkgs, inputs, ... }:
-{
-
-  environment.systemPackages = with pkgs; [
-    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
-    # ... maybe other stuff
-  ];
-
-  home-manager.users.kat5 = {
-    # import the home manager module
-    imports = [
-      inputs.noctalia.homeModules.default
-    ];
-
-    # configure options
-
+{ lib, pkgs, inputs, ... }:
+let
+  userImports = [ inputs.noctalia.homeModules.default ];
+  userSettings = {
     programs.noctalia-shell = {
       enable = true;
       settings = builtins.fromJSON (builtins.readFile ./noctalia-settings.json);
     };
   };
+in
+{
+  environment.systemPackages = with pkgs; [
+    inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
+
+  home-manager.users.kat5 = lib.mkMerge [
+    { imports = userImports; }
+    userSettings
+  ];
 }
