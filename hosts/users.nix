9ed51c4f51939322491c5e4f4f557d1bebe0d405
eepykat <168898505+eepykat@users.noreply.github.com>
2026-06-16 12:52:40 +0300

    users

--- hosts/users.nix
+++ hosts/users.nix
@@ -2,10 +2,14 @@
 { config, pkgs, ... }:
 {
   users.users.kat5 = {
+    isSystemUser = false;
     isNormalUser = true;
     description = "Katarina";
     group = "kat5";
     extraGroups = [ "networkmanager" "wheel" "podman" ];
     packages = with pkgs; [];
   };
+
+  users.groups.kat5 = {};
+  
 }
