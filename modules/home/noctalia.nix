d721e616134f086cc13422dff0285536375f78cf
eepykat <168898505+eepykat@users.noreply.github.com>
2026-06-18 15:27:19 +0300

    meow

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -1,15 +1,11 @@
 { pkgs, inputs, ... }:
 {
-  home-manager.users.kat5 = {
-    # import the home manager module
-    imports = [
-      inputs.noctalia.homeModules.default
-    ];
-
-    # configure options
-    programs.noctalia-shell = {
-      enable = true;
-      settings = builtins.fromJSON (builtins.readFile ./noctalia-settings.json);
-    };
+  imports = [
+    inputs.noctalia.homeModules.default
+  ];
+  
+  programs.noctalia-shell = {
+    enable = true;
+    settings = builtins.fromJSON (builtins.readFile ./noctalia-settings.json);
   };
 }
