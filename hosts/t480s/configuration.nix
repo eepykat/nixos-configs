776d17064e92d560aea9a1561944201ee3126c29
eepykat <168898505+eepykat@users.noreply.github.com>
2026-06-18 17:50:37 +0300

    vpn

--- hosts/t480s/configuration.nix
+++ hosts/t480s/configuration.nix
@@ -11,8 +11,31 @@
 
   networking.hostName = "kat-t480s";
   networking.networkmanager.enable = true;
-  networking.firewall.allowedTCPPorts = [ 53317 ];
-  networking.firewall.allowedUDPPorts = [ 53317 ];
+#  networking.firewall.allowedTCPPorts = [ 53317 ];
+#  networking.firewall.allowedUDPPorts = [ 53317 ];
+
+  #VPN
+  networking.wg-quick.interfaces = {
+    home = {
+      configFile = "/etc/nixos/wireguard/home.conf";
+      autostart = false;
+    };
+    lithuania = {
+      configFile = "/etc/nixos/wireguard/lithuania.conf";
+      autostart = true;
+    };
+    iceland = {
+      configFile = "/etc/nixos/wireguard/iceland.conf";
+      autostart = false;
+    };
+  };
+  networking.firewall.checkReversePath = false;
+
+  # Aditional Hardware
+  hardware.bluetooth.enable = false; # Bluetooth is bad for you >:C
+
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
